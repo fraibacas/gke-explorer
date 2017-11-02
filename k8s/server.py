22a3a028bdaee9b49882d6489000f152111d7d9e
Paco Ibanez-Castillo <fraibacas@gmail.com>
2017-11-02 17:02:15 -0500

    kind of working with gce ingress controller

---
+++ k8s/server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+ 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import datetime
+import os
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        message = "HELLO FROM <h1>{}</h1> MICROSERVICE!".format(os.environ.get("MICROSERVICE", "UNKNOWN"))
+        message += "<br/><br/> {}".format(datetime.datetime.now().isoformat())
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+ 
+def run():
+  server_address = ('', 8080)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
+
+run()
